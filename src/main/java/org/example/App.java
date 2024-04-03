1017b51fa9ef5d933aa35b2d35633576803fe411
泣枫伤竹 <1744350486@qq.com>
2024-04-03 21:57:52 +0800

    change project

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println(App.class.getDeclaredClasses());
         System.out.println( "Hello World!" );
+        System.out.println(App.class.getDeclaringClass());
     }
 }
