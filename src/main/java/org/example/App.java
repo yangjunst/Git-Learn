f7fe40071c6c8fab90e552da9a7be37c8c30d9ca
雪域青竹 <yangjunst@sina.com>
2024-04-03 22:02:24 +0800

    change file

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -13,6 +13,8 @@ public class App
         System.out.println(App.class.getDeclaredClasses());
         System.out.println( "Hello World!" );
         System.out.println(App.class.getDeclaringClass());
+        System.out.println(Object.class.getSimpleName());
         System.out.println(File.class.getName());
+        System.out.println(Object.class.getName());
     }
 }
